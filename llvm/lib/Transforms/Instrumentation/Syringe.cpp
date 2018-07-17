af1b455768adb56462cb96f35e741374cc7a0226
Paul Kirth <paulkirth@google.com>
2018-07-16 17:27:41 -0700

    Start addign the Syringe pass

---
+++ llvm/lib/Transforms/Instrumentation/Syringe.cpp
@@ -0,0 +1,49 @@
+#include "llvm/IR/Attributes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+class SyringePass : public ModulePass {
+public:
+  /// pass identification
+  static char ID;
+
+  SyringePass() : ModulePass(ID) {}
+  virtual ~SyringePass();
+
+  /// Specify pass name for debug output
+  StringRef getPassName() const override { return "Syringe Instrumentation"; }
+  bool runOnModule(Module &M) override {
+    if (skipModule(M)) {
+      return false;
+    }
+    return doBehaviorInjectionForModule(M);
+  }
+
+  bool doBehaviorInjectionForModule(Module &M)
+  {
+      bool ret = false;
+      for(auto fn_it = M.begin(); fn_it != M.end(); fn_it++)
+      {
+          if(fn_it->hasFnAttribute(Attribute::SyringeInjectionSite))
+          {
+              ret= true;
+              //create global function pointer
+              auto globals = M.getGlobalList();
+              GlobalValue gv(type, valuety, use, numops, linkage, name,addrsapce);
+              //clone function
+              //replace original body w/ indirect call
+          }
+      }
+
+
+
+      return ret;
+  }
+
+private:
+  /* data */
+};
