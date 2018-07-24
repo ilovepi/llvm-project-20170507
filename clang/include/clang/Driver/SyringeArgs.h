1d99323a28cc53c39b2d9bb0fbdbca87888091e8
Paul Kirth <paulkirth@google.com>
2018-07-24 16:55:15 -0700

    Add missing files for previous commit

---
+++ clang/include/clang/Driver/SyringeArgs.h
@@ -0,0 +1,37 @@
+//===--- XRayArgs.h - Arguments for XRay ------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef LLVM_CLANG_DRIVER_SYRINGEARGS_H
+#define LLVM_CLANG_DRIVER_SYRINGEARGS_H
+
+#include "clang/Driver/Types.h"
+#include "llvm/Option/Arg.h"
+#include "llvm/Option/ArgList.h"
+
+namespace clang {
+namespace driver {
+
+class ToolChain;
+
+class SyringeArgs {
+  bool SyringeInject = false;
+  bool SyringeRT = true;
+
+public:
+  /// Parses the Syringe arguments from an argument list.
+  SyringeArgs(const ToolChain &TC, const llvm::opt::ArgList &Args);
+  void addArgs(const ToolChain &TC, const llvm::opt::ArgList &Args,
+               llvm::opt::ArgStringList &CmdArgs, types::ID InputType) const;
+
+  bool needsSyringeRt() const { return SyringeInject && SyringeRT; }
+};
+
+} // namespace driver
+} // namespace clang
+
+#endif // LLVM_CLANG_DRIVER_SYRINGEARGS_H
