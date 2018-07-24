d0d748ed62abfcc898a338027085cbe50a09e088
Paul Kirth <paulkirth@google.com>
2018-07-23 17:48:40 -0700

    Update compiler-rt for Syringe runtime

---
+++ compiler-rt/include/syringe/syringe_rt.h
@@ -0,0 +1,34 @@
+#ifndef SYRINGE_RT_H_
+#define SYRINGE_RT_H_ 1
+
+#include <algorithm>
+#include <vector>
+
+struct InjectionData {
+  void *orig_func;
+  void *stub_impl;
+  void *detour_func;
+  void *impl_ptr;
+  template <typename T>
+  InjectionData(T *orig_func, T *stub_impl, T *detour_func, T *impl_ptr)
+      : orig_func(orig_func), stub_impl(stub_impl), detour_func(detour_func),
+        impl_ptr(impl_ptr) {}
+};
+
+class SyringeRT {
+public:
+  SyringeRT() = default;
+  virtual ~SyringeRT() = default;
+  std::vector<InjectionData> data;
+  template <typename T> bool ToggleImpl(T *orig_func);
+  template <typename T> bool ChangeImpl(T *orig_func, T *new_impl);
+
+  template <typename T>
+  void RegisterInjection(T *orig_func, T *stub_impl, T *detour_func, T *impl_ptr);
+
+  template <typename T> InjectionData *FindImplPointer(T *orig_func) ;
+
+private:
+};
+
+#endif /* ifndef SYRINGE_RT_H_ */
