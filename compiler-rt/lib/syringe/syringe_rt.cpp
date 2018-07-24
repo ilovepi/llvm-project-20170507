fb522e65ba81b8f31d5fd78a96b8848d7cef4a6a
Paul Kirth <pk1574@gmail.com>
2018-07-24 08:53:37 -0700

    update compiler-rt build files for syinge runtime

--- compiler-rt/lib/syringe/syringe_rt.cpp
+++ compiler-rt/lib/syringe/syringe_rt.cpp
@@ -1,37 +1,41 @@
-#include "llvm/syringe_rt.h"
+#include "syringe/syringe_rt.h"
 
-  std::vector<InjectionData> data;
-  template <typename T> bool SyringeRT::ToggleImpl(T *orig_func) {
-    auto ptr = FindImplPointer(orig_func);
-    if (!ptr) {
-      return false;
-    }
+namespace __syringe {
 
-    if (ptr->impl_ptr == ptr->stub_impl) {
-      ptr->impl_ptr = ptr->detour_func;
-    } else {
-      ptr->impl = ptr->stub_impl;
-    }
-    return true;
+std::vector<InjectionData> data;
+template <typename T> bool SyringeRT::ToggleImpl(T *orig_func) {
+  auto ptr = FindImplPointer(orig_func);
+  if (!ptr) {
+    return false;
   }
 
-  template <typename T> bool SyringRT::ChangeImpl(T *orig_func, T *new_impl) {
-    auto ptr = FindImplPointer(orig_func);
-    if (!ptr) {
-      return false;
-    }
+  if (ptr->impl_ptr == ptr->stub_impl) {
+    ptr->impl_ptr = ptr->detour_func;
+  } else {
+    ptr->impl = ptr->stub_impl;
+  }
+  return true;
+}
 
-    ptr->impl_ptr = new_impl;
-    return true;
+template <typename T> bool SyringeRT::ChangeImpl(T *orig_func, T *new_impl) {
+  auto ptr = FindImplPointer(orig_func);
+  if (!ptr) {
+    return false;
   }
 
-  template <typename T>
-  void SyringRT::RegisterInjection(T *orig_func, T *stub_impl, T *detour_func, T *impl_ptr) {
-    data.emplace_back(orig_func, stub_impl, detour_func, impl_ptr);
-  }
+  ptr->impl_ptr = new_impl;
+  return true;
+}
 
-  template <typename T> InjectionData *SyringRT::FindImplPointer(T *orig_func) {
-    std::find_if(data.begin(), data.end(),
-                 [orig_func](InjectionData it) -> bool {
-                   return it.orig_func == orig_func;
-                 });
+template <typename T>
+void SyringeRT::RegisterInjection(T *orig_func, T *stub_impl, T *detour_func,
+                                  T *impl_ptr) {
+  data.emplace_back(orig_func, stub_impl, detour_func, impl_ptr);
+}
+
+template <typename T> InjectionData *SyringeRT::FindImplPointer(T *orig_func) {
+  std::find_if(data.begin(), data.end(), [orig_func](InjectionData it) -> bool {
+    return it.orig_func == orig_func;
+  });
+}
+} // end namespace __syringe
