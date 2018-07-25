ebbbb70724a0eb397f7e5c3254a096869681f9e9
Paul Kirth <paulkirth@google.com>
2018-07-25 15:13:30 -0700

    Change Syringe Runtime to avoid using a class
    The Runtime really only neeeds some very basic data, primarily a list of target
    function pointers, and the addresses of their alternate implementations.
    Additionally, it will be necessary to provide some locking/synchronization
    mechanisms for each implementation. A class interface is not necesary, and
    complicates the architecture of the runtime.

--- compiler-rt/lib/syringe/syringe_rt.cpp
+++ compiler-rt/lib/syringe/syringe_rt.cpp
@@ -1,41 +1,10 @@
-#include "syringe/syringe_rt.h"
+
+#include <vector>
+
+#include "syringe/injection_data.h"
 
 namespace __syringe {
 
-std::vector<InjectionData> data;
-template <typename T> bool SyringeRT::ToggleImpl(T *orig_func) {
-  auto ptr = FindImplPointer(orig_func);
-  if (!ptr) {
-    return false;
-  }
+std::vector<InjectionData> global_syringe_data;
 
-  if (ptr->impl_ptr == ptr->stub_impl) {
-    ptr->impl_ptr = ptr->detour_func;
-  } else {
-    ptr->impl = ptr->stub_impl;
-  }
-  return true;
-}
-
-template <typename T> bool SyringeRT::ChangeImpl(T *orig_func, T *new_impl) {
-  auto ptr = FindImplPointer(orig_func);
-  if (!ptr) {
-    return false;
-  }
-
-  ptr->impl_ptr = new_impl;
-  return true;
-}
-
-template <typename T>
-void SyringeRT::RegisterInjection(T *orig_func, T *stub_impl, T *detour_func,
-                                  T *impl_ptr) {
-  data.emplace_back(orig_func, stub_impl, detour_func, impl_ptr);
-}
-
-template <typename T> InjectionData *SyringeRT::FindImplPointer(T *orig_func) {
-  std::find_if(data.begin(), data.end(), [orig_func](InjectionData it) -> bool {
-    return it.orig_func == orig_func;
-  });
-}
 } // end namespace __syringe
