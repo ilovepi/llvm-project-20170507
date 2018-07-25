6d0155ace54263be77ad4a777cb7d22a280fdea2
Paul Kirth <paulkirth@google.com>
2018-07-25 10:33:50 -0700

    Try to get the syringe runtime to resolve correctly

--- syringe-test/main.cpp
+++ syringe-test/main.cpp
@@ -1,19 +1,21 @@
 #include "syringe.hpp"
 #include "util.hpp"
+
 #include <syringe/syringe_rt.h>
 
 using namespace __syringe;
 
-int main() {
-  // SyringeRT s;
-  // s.RegisterInjection(hello, hello_syringe_impl, injected,
-  // _Z17hello_syringe_ptr);
-  init(false);
-  hello();
-  goodbye();
+int main()
+{
+    SyringeRT s;
+    s.RegisterInjection(hello, hello_syringe_impl, injected, _Z17hello_syringe_ptr);
+    init(false);
+    hello();
+    goodbye();
 
-  init(true);
-  hello();
-  goodbye();
-  return 0;
+    init(true);
+    hello();
+    goodbye();
+
+    return 0;
 }
