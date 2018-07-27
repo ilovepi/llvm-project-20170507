5274a0fe2eecafdc6d7dc53f59e7827a266efdad
Paul Kirth <paulkirth@google.com>
2018-07-27 16:25:32 -0700

    Try to add initialization calls in syringe pass
    we need to register the new function bindings in the pass, so it would
    be nice if we could use the  syringe pass to automate this.

--- compiler-rt/include/syringe/syringe_rt.h
+++ compiler-rt/include/syringe/syringe_rt.h
@@ -1,25 +1,29 @@
 #ifndef SYRINGE_RT_H_
 #define SYRINGE_RT_H_ 1
 
+#include "syringe/injection_data.h"
+
 #include <algorithm>
 #include <vector>
-
-#include "syringe/injection_data.h"
 
 namespace __syringe {
 
 extern std::vector<InjectionData> global_syringe_data;
-
-template <typename T, typename R>
-void RegisterInjection(T orig_func, T stub_impl, T detour_func, R impl_ptr) {
-  global_syringe_data.emplace_back(orig_func, stub_impl, detour_func, impl_ptr);
-}
 
 template <typename T> InjectionData *FindImplPointer(T orig_func) {
   return &*std::find_if(global_syringe_data.begin(), global_syringe_data.end(),
                         [orig_func](InjectionData it) -> bool {
                           return it.orig_func == orig_func;
                         });
+}
+
+template <typename T, typename R>
+void RegisterInjection(T orig_func, T stub_impl, T detour_func, R impl_ptr) {
+  auto it = FindImplPointer(orig_func);
+  if (it == nullptr) {
+    global_syringe_data.emplace_back(orig_func, stub_impl, detour_func,
+                                     impl_ptr);
+  }
 }
 
 template <typename T> bool ToggleImpl(T orig_func) {
@@ -47,4 +51,11 @@ template <typename T> bool ChangeImpl(T orig_func, T new_impl) {
 }
 
 } // end namespace __syringe
+
+//extern "C" {
+
+//void __syringe_register(void *orig_func, void *stub_impl, void *detour_func,
+                        //void **impl_ptr);
+//}
+
 #endif /* ifndef SYRINGE_RT_H_ */
--- compiler-rt/lib/syringe/syringe_rt.cpp
+++ compiler-rt/lib/syringe/syringe_rt.cpp
@@ -2,9 +2,18 @@
 #include <vector>
 
 #include "syringe/injection_data.h"
+//#include "syringe/syringe_rt.h"
 
 namespace __syringe {
 
 std::vector<InjectionData> global_syringe_data;
 
 } // end namespace __syringe
+
+//extern "C" {
+
+//void __syringe_register(void *orig_func, void *stub_impl, void *detour_func,
+                        //void **impl_ptr) {
+    //__syringe::RegisterInjection(orig_func, stub_impl, detour_func, impl_ptr);
+//}
+//}// end extern
