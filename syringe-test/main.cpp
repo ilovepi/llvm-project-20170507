3d471c68df11b6ad52466264aa6ab2cc87e5f1e9
Paul Kirth <pk1574@gmail.com>
2018-07-28 15:13:34 -0700

    Try to test registration w/ ctor

--- syringe-test/main.cpp
+++ syringe-test/main.cpp
@@ -8,7 +8,7 @@ using namespace __syringe;
 int main()
 {
     //RegisterInjection(hello, hello_syringe_impl, injected, &_Z17hello_syringe_ptr);
-    __syringe_register(hello, hello_syringe_impl, injected, &_Z17hello_syringe_ptr);
+    //__syringe_register((void*)hello,(void*) hello_syringe_impl,(void*) injected,(void**) &_Z17hello_syringe_ptr);
     init(false);
     hello();
     goodbye();
