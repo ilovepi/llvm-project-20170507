940340b205233067f0132528c865bc5542b96a1a
Paul Kirth <pk1574@gmail.com>
2018-07-30 17:22:07 -0700

    Add assert to ensure that registration is working
    On OSX the registration function is not properly working, and is not
    adding the registration info to the global vector before main.
    
    This makes sure that we know if the vector has not been inserted into
    by the time main starts

--- syringe-test/main.cpp
+++ syringe-test/main.cpp
@@ -2,11 +2,13 @@
 #include "util.hpp"
 
 #include <syringe/syringe_rt.h>
+#include <cassert>
 
 using namespace __syringe;
 
 int main()
 {
+    assert(!__syringe::global_syringe_data.empty());
     //RegisterInjection(hello, hello_syringe_impl, injected, &_Z17hello_syringe_ptr);
     //__syringe_register((void*)hello,(void*) hello_syringe_impl,(void*) injected,(void**) &_Z17hello_syringe_ptr);
     //init(false);
