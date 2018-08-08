ad51f80a32f182204c73aa3b30bbe59f79936ee0
Paul Kirth <paulkirth@google.com>
2018-08-08 10:38:20 -0700

    Revert changes to namespace and InjectionData definition

--- compiler-rt/include/syringe/injection_data.h
+++ compiler-rt/include/syringe/injection_data.h
@@ -2,11 +2,24 @@
 #define SYRINGE_INJECTION_DATA_H 1
 
 #include <stddef.h>
-namespace __syringe {
 
+// Outside of namespace for C compatibility
 typedef void (*fptr_t)(void);
 
-struct InjectionData ;
+namespace __syringe {
+
+struct InjectionData {
+  fptr_t OrigFunc;
+  fptr_t StubImpl;
+  fptr_t DetourFunc;
+  fptr_t *ImplPtr;
+  template <typename T, typename R>
+  InjectionData(T OrigFunction, T StubImplementation, T DetourFunction,
+                R ImplPointer)
+      : OrigFunc((fptr_t)OrigFunction), StubImpl((fptr_t)StubImplementation),
+        DetourFunc((fptr_t)DetourFunction), ImplPtr((fptr_t *)ImplPointer) {}
+};
+
 struct mPtrTy {
   ptrdiff_t ptr;
   ptrdiff_t adj;
