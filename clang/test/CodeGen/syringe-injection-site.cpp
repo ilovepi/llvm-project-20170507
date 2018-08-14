c96314e0eb52d01f59aabf1fd2620a47e4299520
Paul Kirth <paulkirth@google.com>
2018-08-14 15:13:27 -0700

    Add checks for more syringe features
    Add additional checks for the impl_ptr, global_ctor, and registry
    funcitons.

--- clang/test/CodeGen/syringe-injection-site.cpp
+++ clang/test/CodeGen/syringe-injection-site.cpp
@@ -1,5 +1,8 @@
 // RUN: %clang_cc1 %s -fsyringe -std=c++11 -x c++ -emit-llvm -o - -triple x86_64-unknown-linux-gnu | FileCheck %s
 
+// CHECK: @"_Z3fooi$syringe_impl_ptr" = dso_local externally_initialized global void (i32)* @"_Z3fooi$syringe_impl"
+// CHECK: @llvm.global_ctors = appending global [1 x { i32, void ()*, i8* }]
+
 // CHECK: @"_Z3fooi$detour_impl" = alias void (i32), void (i32)* @_Z3bari
 
 // Make sure that the LLVM attribute for XRay-annotated functions do show up.
@@ -15,6 +18,14 @@
 };
 
 // CHECK: define void @"_Z3fooi$syringe_impl"(i32) #2
+
+
+// CHECK: define internal void @"syringe.module_ctor{{.*}}syringe-injection-site.cpp"()
+// CHECK-NEXT: call void @__syringe_register(void (i32)* @_Z3fooi, void (i32)* @"_Z3fooi$syringe_impl", void (i32)* @_Z3bari, void (i32)** @"_Z3fooi$syringe_impl_ptr")
+// CHECK-NEXT: ret void
+
+// CHECK: declare void @__syringe_register(void (i32)*, void (i32)*, void (i32)*, void (i32)**)
+
 
 // CHECK: #0 = {{.*}}"syringe-injection-site"{{.*}}
 // CHECK: #1 = {{.*}}"syringe-payload"{{.*}}"syringe-target-function"="_Z3fooi"
