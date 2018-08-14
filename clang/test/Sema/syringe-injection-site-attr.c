b95c2a1278d3e8eeeb6b3c6e73a9bb3acd8cdb0c
Paul Kirth <paulkirth@google.com>
2018-08-14 12:11:47 -0700

    Move the attribute to match xray example

--- clang/test/Sema/syringe-injection-site-attr.c
+++ clang/test/Sema/syringe-injection-site-attr.c
@@ -1,5 +1,5 @@
 // RUN: %clang_cc1 %s -verify -fsyntax-only -std=c11
-__attribute__((syringe_injection_site)) void foo() ;
+void foo() __attribute__((syringe_injection_site)) ;
 
 struct __attribute__((syringe_injection_site)) a { int x; }; // expected-warning {{'syringe_injection_site' attribute only applies to functions and Objective-C methods}}
 
