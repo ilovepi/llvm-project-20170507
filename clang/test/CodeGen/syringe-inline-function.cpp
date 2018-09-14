3fa5e33e015cae80f246f871d6ce90c49cdbc26b
Paul Kirth <paulkirth@google.com>
2018-09-14 14:42:27 -0700

    Update lit tests to use direct calls

--- clang/test/CodeGen/syringe-inline-function.cpp
+++ clang/test/CodeGen/syringe-inline-function.cpp
@@ -2,9 +2,9 @@
 // RUN: %clang_cc1 %s -fsyringe -std=c++11 -x c++ -emit-llvm -o - -triple x86_64-unknown-linux-gnu | FileCheck %s
 
 [[clang::syringe_injection_site]] int __attribute((always_inline)) foo(int a) {
-  // CHECK: %0 = load i32 (i32)*, i32 (i32)** @"_Z3fooi$syringe_impl_ptr"
-  // CHECK-NEXT: tail call i32 %0(i32 %a) #0
-  // CHECK-NEXT: ret i32 %1
+  // CHECK: %"_Z3fooi$syringe_bool" = load i8, i8* @"_Z3fooi$syringe_bool"
+  // CHECK_NEXT: %0 = trunc i8 %"_Z3fooi$syringe_bool" to i1
+  // CHECK_NEXT: br i1 %0, label %syringe_inject, label %entry
   return a + 1;
 }
 
@@ -13,14 +13,36 @@
   return a - 1;
 }
 
-// CHECK-NOT: call i32 @_Z3foov()
-// CHECK: %[[REG1:[0-9]+]] = load i32 (i32)*, i32 (i32)** @"_Z3fooi$syringe_impl_ptr"
-// CHECK-NEXT: %[[REG2:[0-9]+]] = call i32 %[[REG1]](i32 1)
-// CHECK-NEXT: store i32 %[[REG2]], i32*
-// CHECK: ret i32
-// CHECK-NOT: call i32 @_Z3foov()
+// CHECK: define i32 @main()
+// CHECK: entry:
+// CHECK:   %retval = alloca i32, align 4
+// CHECK:   %j = alloca i32, align 4
+// CHECK:   store i32 0, i32* %retval, align 4
+// CHECK:   %savedstack = call i8* @llvm.stacksave()
+// CHECK:   %"_Z3fooi$syringe_bool.i" = load i8, i8* @"_Z3fooi$syringe_bool"
+// CHECK:   %0 = trunc i8 %"_Z3fooi$syringe_bool.i" to i1
+// CHECK:   br i1 %0, label %syringe_inject.i, label %entry.i
+// CHECK: entry.i:
+// CHECK:   %a.addr.i = alloca i32, align 4
+// CHECK:   store i32 1, i32* %a.addr.i, align 4
+// CHECK:   %1 = load i32, i32* %a.addr.i, align 4
+// CHECK:   %add.i = add nsw i32 %1, 1
+// CHECK:   call void @llvm.stackrestore(i8* %savedstack)
+// CHECK:   br label %_Z3fooi.exit
+// CHECK: syringe_inject.i:
+// CHECK:   %2 = call i32 @_Z3bari(i32 1) #3
+// CHECK:   call void @llvm.stackrestore(i8* %savedstack)
+// CHECK:   br label %_Z3fooi.exit
+// CHECK: _Z3fooi.exit:
+// CHECK:   %call1 = phi i32 [ %add.i, %entry.i ], [ %2, %syringe_inject.i ]
+// CHECK:   store i32 %call1, i32* %j, align 4
+// CHECK:   %3 = load i32, i32* %j, align 4
+// CHECK:   ret i32 %3
+
+
 
 int main() {
   auto j = foo(1);
   return j;
 }
+///usr/local/google/home/paulkirth/workspace/llvm-dev/build/bin/clang -cc1 -internal-isystem /usr/local/google/home/paulkirth/workspace/llvm-dev/build/lib/clang/8.0.0/include -nostdsysteminc /usr/local/google/home/paulkirth/workspace/llvm-dev/clang/test/CodeGen/syringe-inline-function.cpp -fsyringe -std=c++11 -x c++ -emit-llvm -o - -triple x86_64-unknown-linux-gnu
